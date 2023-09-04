df1f9b8ca29bb33d5887284afaa69c66a1ad41c2
jthou20121212 <jthou20121212@gmail.com>
2023-09-04 14:34:51 +0800

    feat: 分享演示 2

--- src/gesture/ShareDemo2.tsx
+++ src/gesture/ShareDemo2.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Dimensions, Image, StatusBar, StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, Image, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const DATA = [
     '台风苏拉一天两次登陆广东',
@@ -33,40 +35,131 @@ const DATA = [
     '专家称A股遍地黄金没人捡'
 ]
 
+const MAX_VELOCITY = 600
+const TRANSLATE_HEIGHT = 100;
+
+/**
+ * 标题和摘要可以滑动 TRANSLATE_HEIGHT
+ * 列表可以滑动 TRANSLATE_HEIGHT + 摘要的高度（summaryHeight）
+ */
 export default function ShareDemo2() {
 
     const { width, height } = Dimensions.get('window');
     const imageHeight = width / 629 * 353;
 
+    const [summaryHeight, setSummaryHeight] = useState(0);
+
+    const translateY = useSharedValue(0);
+    const flatListTranslateY = useSharedValue(0);
+    const scrollHeight = useSharedValue(0);
+
+    const translateAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateY: translateY.value }
+            ]
+        }
+    })
+
+    const flatListTranslateAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateY: flatListTranslateY.value }
+            ]
+        }
+    })
+
+    const tapGestrue = Gesture.Tap()
+        .onTouchesMove((e, manager) => {
+            const flatListCanScrollHeight = summaryHeight + TRANSLATE_HEIGHT
+            // 如果外部容器已经滑动到顶部
+            // case1 继续往上滑让 FlatList 响应手势，表现是正常的
+            // case2 往下滑外部容器和 FlatList 会同时响应事件，外部容器只响应不处理（不改变偏移量）
+            if (flatListTranslateY.value === -flatListCanScrollHeight) {
+                manager.fail();
+
+                console.log('jthou', 'fail');
+            } else {
+                manager.activate();
+                console.log('jthou', 'ative');
+            }
+        })
+
+    const scrollGestrue = Gesture.Native()
+        .requireExternalGestureToFail(tapGestrue);
+
+    const panGesture = Gesture.Pan()
+        .onChange((e) => {
+            // 如果 FlatList 有滑动不处理
+            if (scrollHeight.value !== 0) return;
+
+            translateY.value = Math.min(0, Math.max(-TRANSLATE_HEIGHT, translateY.value + e.changeY));
+
+            const flatListCanScrollHeight = summaryHeight + TRANSLATE_HEIGHT
+            flatListTranslateY.value = Math.min(0, Math.max(-flatListCanScrollHeight, flatListTranslateY.value + e.changeY));;
+        })
+        .onEnd((e) => {
+            if (Math.abs(e.velocityY) < MAX_VELOCITY) return;
+            // if (scrollHeight.value !== 0) return;
+
+            if (e.velocityY > 0) {
+                flatListTranslateY.value = withTiming(0);
+                translateY.value = withTiming(0);
+
+                console.log('jthou', "> 0");
+            } else {
+                translateY.value = withTiming(-TRANSLATE_HEIGHT);
+                flatListTranslateY.value = withTiming(-(TRANSLATE_HEIGHT + summaryHeight));
+
+                console.log('jthou', "<= 0");
+            }
+        })
+        .simultaneousWithExternalGesture(tapGestrue, scrollGestrue);
+
+    const handleScroll = useAnimatedScrollHandler({
+        onScroll: e => {
+            scrollHeight.value = e.contentOffset.y;
+        }
+    })
+
     return (
-        <View style={styles.container} >
-            <StatusBar translucent={true} backgroundColor={'transparent'} />
-            <View style={{ justifyContent: 'flex-end' }} >
-                <Image
-                    style={[
-                        {
-                            width: width,
-                            height: imageHeight,
-                        },
-                    ]}
-                    source={{ uri: 'https://img.huxiucdn.com/test/img/pro_banner/202306/05/142434788388.jpeg' }}
-                />
-                <Text style={[styles.title]} >邀酒摧肠三杯醉</Text>
+        <GestureDetector gesture={panGesture} >
+            <View style={styles.container} >
+                <StatusBar translucent={true} backgroundColor={'transparent'} />
+                <View style={{ justifyContent: 'flex-end' }} >
+                    <Image
+                        style={[
+                            {
+                                width: width,
+                                height: imageHeight,
+                            },
+                        ]}
+                        source={{ uri: 'https://img.huxiucdn.com/test/img/pro_banner/202306/05/142434788388.jpeg' }}
+                    />
+                    <Animated.Text style={[styles.title, translateAnimatedStyle]} >邀酒摧肠三杯醉</Animated.Text>
+                </View>
+                <Animated.Text style={[styles.summary, translateAnimatedStyle]}
+                    onLayout={(event) => {
+                        const { x, y, width, height } = event.nativeEvent.layout;
+                        setSummaryHeight(height);
+                    }}
+                >
+                    很多人都设想过35岁失业后的去向，“可以开网约车、送快递”。然而，当越来越多的人进入这些行业，原本被视为“退路”的选择开始“内卷”起来了。
+                    网约车市场逐渐饱和。今年多地网约车日均接单量不足10单，三亚、长沙暂停受理网约车运输证新增业务。网约车司机“月入过万”的神话，要终结了？中年人去哪再就业？
+                </Animated.Text>
+                <GestureDetector gesture={Gesture.Simultaneous(tapGestrue, scrollGestrue)} >
+                    <Animated.FlatList
+                        bounces={false}
+                        style={[{ height: height, marginBottom: -(summaryHeight + TRANSLATE_HEIGHT) }, flatListTranslateAnimatedStyle]}
+                        data={DATA}
+                        renderItem={({ item, index }) => <Text style={styles.text} >{item}</Text>}
+                        ItemSeparatorComponent={() => <View style={styles.separator} />}
+                        keyExtractor={item => item.toString()}
+                        onScroll={handleScroll}
+                    />
+                </GestureDetector>
             </View>
-            <Text style={[styles.summary]}
-            >
-                很多人都设想过35岁失业后的去向，“可以开网约车、送快递”。然而，当越来越多的人进入这些行业，原本被视为“退路”的选择开始“内卷”起来了。
-                网约车市场逐渐饱和。今年多地网约车日均接单量不足10单，三亚、长沙暂停受理网约车运输证新增业务。网约车司机“月入过万”的神话，要终结了？中年人去哪再就业？
-            </Text>
-            <FlatList
-                    bounces={false}
-                    style={[{ height: height, marginBottom: 0 }]}
-                    data={DATA}
-                    renderItem={({ item, index }) => <Text style={styles.text} >{item}</Text>}
-                    ItemSeparatorComponent={() => <View style={styles.separator} />}
-                    keyExtractor={item => item.toString()}
-                />
-        </View>
+        </GestureDetector>
     );
 };
 
@@ -79,7 +172,8 @@ const styles = StyleSheet.create({
         lineHeight: 46,
         fontSize: 16,
         color: 'black',
-        textAlign: 'center'
+        textAlign: 'center',
+        backgroundColor: '#eeeeee'
     },
     separator: {
         height: 1,
